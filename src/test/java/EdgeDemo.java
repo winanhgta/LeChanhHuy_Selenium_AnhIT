f96bd210e92e4919ca70c68a6947de180e75e67f
winan <winanhgta393@gmail.com>
2025-10-30 22:37:31 +0700

    First commit

--- src/test/java/EdgeDemo.java
+++ src/test/java/EdgeDemo.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
 
 public class EdgeDemo {
-}
+        private static WebDriver driver;
+
+        public static void main (String[] args){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://automationexercise.com/");
+        }
+    }
+
