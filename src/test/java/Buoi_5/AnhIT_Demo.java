4bec05d16f7012f4fd8728ae60d048cbc39b60c9
winan <winanhgta393@gmail.com>
2025-11-10 14:31:44 +0700

    Buoi 5

--- src/test/java/Buoi_5/AnhIT_Demo.java
+++ src/test/java/Buoi_5/AnhIT_Demo.java
@@ -6,4 +6,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AnhIT_Demo {
+    public static void main(String[] args) {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://anhitsolutions.com/login");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement emailInput = driver.findElement(By.xpath("//input[@name='username']"));
+        emailInput.sendKeys("lechanhhuy03@gmail.com");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@name='password']"));
+        passwordInput.sendKeys("chanhhuy393");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement login = driver.findElement(By.xpath("//button[@type='submit']"));
+        login.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
