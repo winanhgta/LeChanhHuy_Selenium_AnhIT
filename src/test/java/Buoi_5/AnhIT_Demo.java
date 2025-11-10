1d57a82144eee2e20a970ad21742869373176200
winan <winanhgta393@gmail.com>
2025-11-10 21:28:18 +0700

    Buoi 5

--- src/test/java/Buoi_5/AnhIT_Demo.java
+++ src/test/java/Buoi_5/AnhIT_Demo.java
@@ -6,41 +6,47 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AnhIT_Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
         driver.get("https://anhitsolutions.com/login");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(2000);
+
 
         WebElement emailInput = driver.findElement(By.xpath("//input[@name='username']"));
-        emailInput.sendKeys("lechanhhuy03@gmail.com");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        String email = "lechanhhuy03@gmail.com";
+        emailInput.sendKeys(email);
+        System.out.println(email);
+        Thread.sleep(2000);
+
 
         WebElement passwordInput = driver.findElement(By.xpath("//input[@name='password']"));
-        passwordInput.sendKeys("chanhhuy393");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        String password = "chanhhuy393";
+        System.out.println(password);
+        passwordInput.sendKeys(password);
+        Thread.sleep(2000);
 
-        WebElement login = driver.findElement(By.xpath("//button[@type='submit']"));
+        WebElement login = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[2]/div/form/button"));
         login.click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(2000);
 
+
+        driver.findElement(By.xpath("//a[@href='/panel/setting']")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//input[@type='tel']")).clear();
+        driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("0782747539");
+
+        driver.findElement(By.xpath("//button[@id='saveData']")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//a[contains(@href, '/panel/setting/step/3')]")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//textarea[@name='about']")).sendKeys("Là một người từng mông lung trong việc tìm hiểu ngành nghề tester, AnhIT đã giúp tôi xác định và hướng dẫn các kiến thức cần thiết để trở thành một tester trong tương lai");
+
+        driver.findElement(By.xpath("//button[@id='saveData']")).click();
     }
 }
