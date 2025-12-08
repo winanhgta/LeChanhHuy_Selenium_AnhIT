dc5d0e04b73b47d915edf4949df351de1f1b5a33
winan <winanhgta393@gmail.com>
2025-12-09 00:20:20 +0700

    Feat: Add class Custom Dropdown OrangeHRM (Unfinished)

--- src/test/java/Buoi_6/OrangeHRM_Dropdown.java
+++ src/test/java/Buoi_6/OrangeHRM_Dropdown.java
@@ -8,16 +8,17 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import java.util.List;
 public class OrangeHRM_Dropdown {
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException {
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
+        Thread.sleep(2000);
         // Đăng nhập vào trang với username và password
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
         //Chuyển sang trang admin
         driver.findElement(By.xpath("//a[@href='/web/index.php/admin/viewAdminModule']")).click();
