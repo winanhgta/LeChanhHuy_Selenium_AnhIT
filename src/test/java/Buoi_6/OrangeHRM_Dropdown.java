71d28e57627abe005e5e57c9ab11d7ed78d9716a
winan <winanhgta393@gmail.com>
2025-12-09 00:05:40 +0700

    Feat: Add class Custom Dropdown OrangeHRM (Unfinished)

---
+++ src/test/java/Buoi_6/OrangeHRM_Dropdown.java
@@ -0,0 +1,28 @@
+package Buoi_6;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import java.util.List;
+public class OrangeHRM_Dropdown {
+    public static void main (String[] args){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        // Đăng nhập vào trang với username và password
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+
+        //Chuyển sang trang admin
+        driver.findElement(By.xpath("//a[@href='/web/index.php/admin/viewAdminModule']")).click();
+
+        //
+
+    }
+}
